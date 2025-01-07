c8a685b7301e60c69c9cadcc73a413559a955c48
Mazen2810 <mazenmuhammed52@gmail.com>
2025-01-08 01:48:23 +0200

    Q1

---
+++ Demo/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    internal struct Person
+    {
+        public string? Name { get; set; }
+        public int Age { get; set; }
+
+        public Person (string Name,int Age)
+        {
+            this.Name = Name;
+            this.Age = Age;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name} \tAge: {Age}";
+        }
+    }
+}
--- Demo/Program.cs
+++ Demo/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1: Define a struct "Person" with properties "Name" and "Age". Create an array of three "Person" objects and populate it with data. Then, write a C# program to display the details of all the persons in the array.
+
+            //Person[] person = new Person[3];
+            //person[0] = new Person("Mazen", 26);
+            //person[1] = new Person("Mohamed", 22);
+            //person[2] = new Person("Mohamed", 21);
+
+            //for (int i = 0; i < person.Length; i++)
+            //{
+            //    Console.WriteLine(person[i].ToString());
+            //}
+            #endregion
         }
     }
 }
