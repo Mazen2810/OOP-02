a73a7325704353f75578bc411224c26fca23f75c
Mazen2810 <mazenmuhammed52@gmail.com>
2025-01-07 19:13:41 +0200

    Indexer

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -8,15 +8,31 @@ namespace Demo
         {
             #region Encapsulation
 
-            Employee employee = new Employee(1001, "Ahmed Nasr", 10_000);
+            //Employee employee = new Employee(1001, "Ahmed Nasr", 10_000);
 
-            employee.SetId(2002); //Set ID
+            //employee.SetId(2002); //Set ID
 
-            employee.Name = "Ibrahim Ali";
-            Console.WriteLine(employee.Name);
-            Console.WriteLine(employee.GetId()); // get Id
+            //employee.Name = "Ibrahim Ali";
+            //Console.WriteLine(employee.Name);
+            //Console.WriteLine(employee.GetId()); // get Id
 
-            Console.WriteLine(employee);
+            //Console.WriteLine(employee);
+            #endregion
+
+            #region Indexer
+            //PhoneBook note = new PhoneBook(10);
+            //note.AddPerson(0, "Ahmed", 111);
+            //note.AddPerson(1, "Nadia", 222);
+            //note.AddPerson(2, "Osama", 333);
+
+            //note.SetNumber("Ahmed", 999);
+            //Console.WriteLine(note.GetPersonNumber("Ahmed"));
+
+            //for (int i = 0; i < note.Size; i++)
+            //{
+            //    Console.WriteLine(note[i]);
+            //}
+            //Console.WriteLine(note.ToString());
             #endregion
         }
     }
