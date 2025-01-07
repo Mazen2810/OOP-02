4258c9cf28106df4a29b88a17f17da5b2fb5910c
Mazen2810 <mazenmuhammed52@gmail.com>
2025-01-07 21:06:39 +0200

    Class

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -34,6 +34,21 @@ namespace Demo
             //}
             //Console.WriteLine(note.ToString());
             #endregion
+            #region Class
+
+            //Car C1;
+            //// Declare for Reference of type "Car"
+            //// this Reference 'C1' can Refer an object of type "Car" or of another type "Inheriting from Car"
+            //// or another type
+            //// CLR will allocate Uninitialized bytes in stack for the Reference
+            //// CLR will allocates 0 bytes in heap
+
+            //C1 = new Car(10102, "Nissan Sentra 2025", 180);
+
+            //C1 = new Car(3333, "KIA");
+
+            //Console.WriteLine(C1);
+            #endregion
         }
     }
 }
