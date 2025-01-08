783ae867aacbc7ea9b71a41eff268907790fc7d9
Mazen2810 <mazenmuhammed52@gmail.com>
2025-01-08 02:35:46 +0200

    Q2

---
+++ Demo/Point.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    internal struct Point
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public Point(double X , double Y)
+        {
+            this.X = X;
+            this.Y = Y;
+        }
+        public double Distance (Point P2)
+        {
+            double xDiff = X - P2.X;
+            double yDiff = Y - P2.Y;
+            return Math.Sqrt(xDiff * xDiff + yDiff * yDiff);
+        }
+    }
+}
--- Demo/Program.cs
+++ Demo/Program.cs
@@ -16,6 +16,31 @@
             //    Console.WriteLine(person[i].ToString());
             //}
             #endregion
+
+
+            #region Q2: Create a struct called "Point" to represent a 2D point with properties "X" and "Y". Write a C# program that takes two points as input from the user and calculates the distance between them.
+           
+            //Point P1 = new Point(GetCoordinates("Enter X-Coordinate for the First Point: "),GetCoordinates("Enter Y-Coordinate for the First Point: "));
+            //Point P2 = new Point(GetCoordinates("Enter X-Coordinate for the Second Point: "), GetCoordinates("Enter Y-Coordinate for the Second Point: "));
+            //double Distance = P1.Distance(P2);
+            //Console.WriteLine($"The Distance between First Point and Second Point = {Distance}");
+
+
+            //static double GetCoordinates(string message)
+            //{
+            //    double value;
+            //    bool flag;
+            //    do
+            //    {
+            //        Console.Write(message);
+            //        flag = double.TryParse(Console.ReadLine(), out value);
+            //    }while (!flag);
+            //    return value;
+            //}
+            #endregion
+
+
+
         }
     }
 }
